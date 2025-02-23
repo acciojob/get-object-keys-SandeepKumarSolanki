210af205b89018629c26c08df878c3c2dbb269f4
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-23 10:53:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const student1 = {
 const student2 = {
 	name : "Amit"
 }
-function getKeys(obj) {
+function findKeys(obj) {
 	return Object.keys(obj);
 }
 
-let keys1 = getKeys(student1);
-let keys2 = getKeys(student2);
+let keys1 = [...findKeys(student1)]
+let keys2 = [...findKeys(student2)]
 console.log(keys1);
 console.log(keys2);
 
