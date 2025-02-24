42eab1489df8e344179858ee31962069ffb609a5
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-24 09:37:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-//your JS code here. If required.
-
-// Create the student object
+// Create the student object with the getKeys method
 const student = {
   name: "John",
   age: 20,
-  city: "New York"
+  city: "New York",
+  getKeys: function() {
+    return Object.keys(this);
+  }
 };
 
-// Function to get all keys from the object
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-
-// Exporting getKeys function if needed for your tests (for example, in Cypress testing)
-window.getKeys = getKeys;
-
-
-
+// Now you can use myObj.getKeys()
+// In your tests
+const myObj = student;  // Assuming myObj is supposed to be the student object
+console.log(myObj.getKeys()); // Output: ["name", "age", "city"]
 
