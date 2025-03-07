0cf953b2060b42ebf5f7a04dfdeed5e95fb2eba0
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-07 10:13:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-// Create the Student class
-class Student {
-  getKeys(obj) {
-    return  Object.keys(obj);
-  }
-}
-
-
-const detail = {
-  name: "dev",
-  age: 21,
-  city: "Lucknow"
-};
-
+// Create the student object
 const student = {
   name: "John",
 };
 
-let myObj = new Student();
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
 
-console.log(myObj.getKeys(student));  
-console.log(myObj.getKeys(detail));   
+// Testing the function
+console.log(getKeys(student));
+
+
+
+// // Create the Student class
+// class Student {
+//   getKeys(obj) {
+//     return  Object.keys(obj);
+//   }
+// }
+// const detail = {
+//   name: "dev",
+//   age: 21,
+//   city: "Lucknow"
+// };
+// const student = {
+//   name: "John",
+// };
+// let myObj = new Student();
+// console.log(myObj.getKeys(student));  
+// console.log(myObj.getKeys(detail));   
