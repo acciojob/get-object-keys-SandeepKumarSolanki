2e9b9101da9e6a63da5d834063584650353e78a2
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-07 10:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,17 +4,22 @@
 // Create the Student class
 class Student {
   getKeys(obj) {
-    return  Object.keys(obj);
+	let arr = [...Object.keys(obj)]
+    console.log(arr);
+	return arr;
   }
 }
+
+const student = {
+  name: "John",
+};
 const detail = {
   name: "dev",
   age: 21,
   city: "Lucknow"
 };
-const student = {
-  name: "John",
-};
+
+
 let myObj = new Student();
 myObj.getKeys(student);  
 myObj.getKeys(detail);   
