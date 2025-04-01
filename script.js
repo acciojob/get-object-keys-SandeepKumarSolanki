9b21bdacdc372ce6235bad474c1799fac129b923
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-01 16:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const info = {
 };
 
 function getKeys(obj) {
-  return Object.keys(obj);  // Correct usage of Object.keys()
+  return Object.keys(this);  // Correct usage of Object.keys()
 }
 
 console.log(getKeys(student));  // ["name"]
